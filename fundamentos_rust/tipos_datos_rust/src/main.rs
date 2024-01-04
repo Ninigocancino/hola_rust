bfe4292b65fd7c527025356093bacf71cc5a5d94
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2024-01-03 22:11:56 -0600

    Se agregó la sección 'números enteros sin signo' al archivo 'main.rs' del paquete de proyecto 'tipos_de_datos_rust'

--- fundamentos_rust/tipos_datos_rust/src/main.rs
+++ fundamentos_rust/tipos_datos_rust/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     
     //DATOS DE TIPO PRIMITIVOS
 
-    /*Número enteros:
+    /*Números enteros con signo:
 
     Rust admite el uso de numeros enteros con signo de 8,16,32,64 y 132 bits respectivamente que se deben representar de la siguiente forma:
 
@@ -14,15 +14,45 @@ fn main() {
     i64  => Enteros de 64 bits
     i132 => Enteros de 132 bits
     
-    Un entero con signo es un tipo de netero que puede representar tanto números positivosa como negativos.
+    Un entero con signo es un tipo de entero que puede representar tanto números positivos como negativos.
 
      */
 
     //Ejemplo  de número entero con signo:
 
+    /* 
     let entero:i32 = -42;
-
     println!("El valor del entero consigno es {}", entero)
+    */
+
+    /*Números enteros sin signo :
+
+    Rust admite el uso de numeros enteros sin signo de 8,16,32,64 y 132 bits respectivamente que se deben representar de la siguiente forma:
+
+    u8   => Enteros de 8 bits
+    u16  => Enteros de 16 bits
+    u32  => Enteros de 32 bits
+    u64  => Enteros de 64 bits
+    u132 => Enteros de 123 bits
+
+    Un entero sin signo es un tipo de entero que solo puede representar números positivos.
+    
+     */
+
+    //Ejemplo de datos de tipo entero sin signo:
+
+    /*
+    let numero_sin_signo: u32 = 42;
+    println!("El valor del entero sin signo es {}", numero_sin_signo)
+    */
+
+    //Si usamos un número negativo la línea de código generará un error al ejecutarse:
+    
+    /* 
+    let numero_sin_signo: u32 = -42;
+    println!("El valor del entero sin signo es {}", numero_sin_signo);
+    */
+
 
 
 }
