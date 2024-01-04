387ad8eaa673331d0d89cc7a71a8d81c02804dde
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2024-01-03 21:53:31 -0600

    Se agregó la 'sección Números enteros con signos' al archivo 'main.rs' dentro del paquete de proyecto 'tipos_de_datos-rust'

--- fundamentos_rust/tipos_datos_rust/src/main.rs
+++ fundamentos_rust/tipos_datos_rust/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+
+    //Conocer los tipos de datos que admite Rust es algo fundamental para el aprendizaje  de este  lenguaje
+    
+    //DATOS DE TIPO PRIMITIVOS
+
+    /*Número enteros:
+
+    Rust admite el uso de numeros enteros con signo de 8,16,32,64 y 132 bits respectivamente que se deben representar de la siguiente forma:
+
+    i8   => Enteros de 8 bits
+    i16  => Enteros de 16 bits
+    i32  => Enteros de 32 bits
+    i64  => Enteros de 64 bits
+    i132 => Enteros de 132 bits
+    
+    Un entero con signo es un tipo de netero que puede representar tanto números positivosa como negativos.
+
+     */
+
+    //Ejemplo  de número entero con signo:
+
+    let entero:i32 = -42;
+
+    println!("El valor del entero consigno es {}", entero)
+
+
 }
